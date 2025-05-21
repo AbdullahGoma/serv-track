b5de989a664abb6abad5ffb9e4b2651cf08ea3a3
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-21 19:26:33 +0300

    Cleaning Up Interval after component closed

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,14 +7,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   // @HostBinding('class') get statusClass() {
   //   return `status-${this.currentStatus}`;
   // }
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
 
   ngOnInit() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const rnd = Math.random(); // 0 to 0.999
 
       if (rnd < 0.5) {
@@ -25,5 +26,9 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
