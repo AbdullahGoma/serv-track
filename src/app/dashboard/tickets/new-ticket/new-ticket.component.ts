57cd676f7b68d305d0fe4530731ef33fd0de8bdb
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-21 20:45:55 +0300

    ngAfterViewInit and ngAfterContentInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from '../../../shared/button/button.component';
@@ -11,12 +11,21 @@ import { ControlComponent } from "../../../shared/control/control.component";
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {
-  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit() {
+    console.log('On Init');
+    console.log(this.form?.nativeElement);
+  }
+  ngAfterViewInit() {
+    console.log('After View Init');
+    console.log(this.form?.nativeElement);
+  }
 
   onSubmit(enteredTitle: string, enteredRequest: string) {
     console.log(enteredTitle, enteredRequest);
-    this.form()?.nativeElement.reset();
+    this.form?.nativeElement.reset();
   }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterContentInit,
   Component,
   contentChild,
   ContentChild,
@@ -20,7 +21,7 @@ import {
     '(click)': 'onClick()',
   },
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
   // @HostBinding('class') className = 'control';
   // @HostListener('click') onClick() {
   //   console.log('Clicked!');
@@ -30,6 +31,10 @@ export class ControlComponent {
   // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
   private control =
     contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  ngAfterContentInit() {
+    
+  }
 
   onClick() {
     console.log('Clicked!');
